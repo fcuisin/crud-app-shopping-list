3ee77a01677328cbf2c1cff75a67905cb24f01f3
Ferdinand Cuisin <fcuisin@gmail.com>
2021-01-20 17:16:41 +0100

    Create fetch function to retrieve data + add seeds

---
+++ script.js
@@ -0,0 +1,29 @@
+const list = [
+  {
+    name:"citrus",
+    number:3,
+  },
+  {
+    name:"pomelos",
+    number:1,
+  }
+]
+
+function fetchAllMovies(list) {
+  const table = document.getElementById("items");
+  const counter = document.getElementById("counter");
+  let data = "";
+  list.forEach(function (item, index) {
+    data += `<tr>
+      <td>${item.name}</td>
+      <td>${item.number}</td>
+      <td><button class="btn-modify" value="${index}">Modify</button></td>
+    </tr>
+`
+
+  });
+
+  table.innerHTML += data;
+}
+
+fetchAllMovies(list);
